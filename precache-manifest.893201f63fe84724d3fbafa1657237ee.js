25f2899300b989566b14fed5c849f9653df7ada0
Yogev Henig <yogevhenig@gmail.com>
2020-04-15 17:00:06 +0300

    Updates

---
+++ precache-manifest.893201f63fe84724d3fbafa1657237ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457e88da5bce2fde7997c4294ae33841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "71957d4d4301f760361e",
+    "url": "/static/css/main.3c9e49ff.chunk.css"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71957d4d4301f760361e",
+    "url": "/static/js/main.c918f9fe.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "4a7dec0a06d210b9ea78686031fc54ce",
+    "url": "/static/media/sega-webfont.4a7dec0a.woff"
+  }
+]);
